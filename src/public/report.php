9f83faca597ac26a37621c6ab6580399a3f1928a
rgrttg <ottiger@outlook.de>
2024-01-30 15:02:59 +0100

    Reports hinzugefügt

---
+++ src/public/report.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Report</title>
+  <link rel="stylesheet" href="assets/css/style.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+<main>
+  <div class="container">
+    <div class="col">
+      <div class="row">
+        <h1>Trivia-Quiz</h1>
+      </div>
+      
+      <!-- Fragen durch nummerieren --------------------------------------- -->
+      <div class="row" id="frageNummer">
+        <div class="mb-3">
+          Auswertung des Quiz'
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+</body>
+
+</html>
